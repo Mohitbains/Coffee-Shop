f89d839dba7f8ab7e477c5d3d813ddc3e7835cfe
Mohitbains <mohitbains861@gmail.com>
2022-06-15 18:47:02 +0530

    fix telegram api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,57 @@
-/* eslint-disable import/no-unresolved */
+/* eslint-disable react/jsx-props-no-spreading */
+import { useForm } from 'react-hook-form';
 import React from 'react';
 
-const Form = () => (
-  <form className="w-full my-12">
-    <div className="">
-      <input
-        className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
-        id="Name"
-        type="text"
-        placeholder="Name"
-      />
-      <input
-        className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
-        id="Email"
-        type="text"
-        placeholder="Email"
-      />
-      <textarea
-        className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
-        rows="4"
-        id="message"
-        type="text"
-        placeholder="Message">
-        {' '}
-      </textarea>
-    </div>
-    <button
-      type="submit"
-      className="px-10 py-3 bg-yellow-900 text-white rounded-full font-semibold">
-      Send Message
-    </button>
-  </form>
-);
+const Form = ({ handleFormSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const onSubmit = data => handleFormSubmit(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full my-12">
+      <div className="">
+        <input
+          className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
+          id="Name"
+          type="text"
+          placeholder="Name"
+          {...register('name', { required: true })}
+          error={errors.name?.type === 'required' && 'Name is required'}
+          onChange={e => setValue('name', e.target.value)}
+        />
+        <input
+          className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
+          id="phone"
+          type="number"
+          placeholder="Phone Number"
+          {...register('phone', { required: true })}
+          error={
+            errors.phone?.type === 'required' && 'Phone Number is required'
+          }
+          onChange={e => setValue('email', e.target.value)}
+        />
+        <input
+          className="mb-4 rounded w-full py-2 px-3 text-black leading-tight"
+          rows="4"
+          id="message"
+          type="text"
+          placeholder="Message"
+          {...register('message', { required: true })}
+          error={errors.message?.type === 'required' && 'Message is required'}
+          onChange={e => setValue('message', e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        className="px-10 py-3 bg-yellow-900 text-white rounded-full font-semibold">
+        Send Message
+      </button>
+    </form>
+  );
+};
 
 export default Form;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import axios from 'axios';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import { Title, Subtitle, MainContainer } from '../components/elements';
@@ -8,57 +9,73 @@ import Form from '../components/Form';
 
 const Section = styled.div``;
 
-const Contact = () => (
-  <Layout>
-    <Seo title="Contact" description="" />
-    <Section>
-      <div className="flex justify-center">
-        <div className="w-full sm:w-1/2">
-          <img className="image" src="/images/contact.png" alt="contact us" />
-        </div>
-      </div>
-      <MainContainer className="text-center">
-        <Title>Get In Touch</Title>
-        <div className="flex flex-wrap pt-16">
+const Contact = () => {
+  const handleSubmit = async val => {
+    await axios
+      .post('https://pick-my-laundry.vercel.app/api/contact', {
+        name: val.name,
+        phone: val.phone,
+        message: val.message,
+      })
+      .then(function(response) {
+        console.log('response', response);
+        alert('Submit Successfuly');
+      })
+      .catch(function(error) {
+        console.log('error', error);
+      });
+  };
+  return (
+    <Layout>
+      <Seo title="Contact" description="" />
+      <Section>
+        <div className="flex justify-center">
           <div className="w-full sm:w-1/2">
-            <Subtitle className="text-yellow-900 font-semibold">
-              Please give us your feedback!
-            </Subtitle>
-            <Subtitle className="pt-4 grey leading-normal">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim.
-            </Subtitle>
-            <Form />
-          </div>
-          <div className="w-full sm:w-1/2 text-center">
-            <Subtitle className="text-yellow-900 font-semibold">
-              <i className="fas fa-map-marker-alt text-4xl text-yellow-900" />
-              Coffee Shop
-            </Subtitle>
-            <Subtitle className="pt-4 grey leading-normal">
-              321 Awesome Stree
-            </Subtitle>
-            <Subtitle className="pt-4 grey leading-normal">
-              New York, NY 12345
-            </Subtitle>
-            <div>
-              <i className="fas fa-envelope text-4xl text-yellow-900 mt-10" />
-            </div>
-            <Subtitle className="pt-4 grey leading-normal">
-              info@companyname.com{' '}
-            </Subtitle>
-            <div>
-              <i className="fas fa-phone text-4xl text-yellow-900 mt-10" />
-            </div>
-            <Subtitle className="pt-4 grey leading-normal">
-              +1 235 123 1234
-            </Subtitle>
+            <img className="image" src="/images/contact.png" alt="contact us" />
           </div>
         </div>
-      </MainContainer>
-    </Section>
-  </Layout>
-);
-
+        <MainContainer className="text-center">
+          <Title>Get In Touch</Title>
+          <div className="flex flex-wrap pt-16">
+            <div className="w-full sm:w-1/2">
+              <Subtitle className="text-yellow-900 font-semibold">
+                Please give us your feedback!
+              </Subtitle>
+              <Subtitle className="pt-4 grey leading-normal">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim.
+              </Subtitle>
+              <Form handleFormSubmit={handleSubmit} />
+            </div>
+            <div className="w-full sm:w-1/2 text-center">
+              <Subtitle className="text-yellow-900 font-semibold">
+                <i className="fas fa-map-marker-alt text-4xl text-yellow-900" />
+                Coffee Shop
+              </Subtitle>
+              <Subtitle className="pt-4 grey leading-normal">
+                321 Awesome Stree
+              </Subtitle>
+              <Subtitle className="pt-4 grey leading-normal">
+                New York, NY 12345
+              </Subtitle>
+              <div>
+                <i className="fas fa-envelope text-4xl text-yellow-900 mt-10" />
+              </div>
+              <Subtitle className="pt-4 grey leading-normal">
+                info@companyname.com{' '}
+              </Subtitle>
+              <div>
+                <i className="fas fa-phone text-4xl text-yellow-900 mt-10" />
+              </div>
+              <Subtitle className="pt-4 grey leading-normal">
+                +1 235 123 1234
+              </Subtitle>
+            </div>
+          </div>
+        </MainContainer>
+      </Section>
+    </Layout>
+  );
+};
 export default Contact;
