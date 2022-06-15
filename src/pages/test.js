005c1c194f4fb4dcd9ffb7cd90f489a473c9fe04
Mohitbains <mohitbains861@gmail.com>
2022-06-15 17:23:35 +0530

    fix

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,24 +4,67 @@ import ContactForm from '../components/form/ContactForm';
 
 const ContactPage = () => {
   const handleFormSubmit = async val => {
-    await axios
-      .post(`https://pick-my-laundry.vercel.app/api/contact`, {
-        name: val.name,
-        phone: val.phoneNumber,
-        message: val.message,
-      })
-      .then(response => {
-        console.log('response', response);
+    // axios.defaults.headers.post['Content-Type'] =
+    //   'application/json;charset=utf-8';
+    // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    // await axios
+    //   .post(`https://pick-my-laundry.vercel.app/api/contact`, {
+    //     name: 'hjhfj',
+    //     phone: 'hjhj',
+    //     message: 'jhjh',
+    //   })
+    //   .then(response => {
+    //     console.log('response', response);
 
-        if (typeof window !== 'undefined') {
-          window.alert('Message sent successfully');
-        }
+    //     if (typeof window !== 'undefined') {
+    //       window.alert('Message sent successfully');
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //     if (typeof window !== 'undefined') {
+    //       window.alert(error);
+    //     }
+    //   });
+    // const data = {
+    //   name: 'hjhfj',
+    //   phone: 'hjhj',
+    //   message: 'jhjh',
+    // };
+    // await axios({
+    //   method: 'post',
+    //   url: 'https://pick-my-laundry.vercel.app/api/contact',
+    //   data,
+    // })
+    //   .then(function(response) {
+    //     console.log(response.data);
+    //     if (typeof window !== 'undefined') {
+    //       window.alert('Message sent successfully');
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //     if (typeof window !== 'undefined') {
+    //       window.alert(error);
+    //     }
+    //   });
+
+    const headers = {
+      'Content-Type': 'text/plain',
+    };
+    const data = {
+      name: 'this.state.user_name',
+      phone: 'this.state.password',
+      message: 'jhhj',
+    };
+
+    await axios
+      .post('https://pick-my-laundry.vercel.app/api/contact', data, { headers })
+      .then(response => {
+        console.log('Success ========>', response);
       })
       .catch(error => {
-        console.log(error);
-        if (typeof window !== 'undefined') {
-          window.alert(error);
-        }
+        console.log('Error ========>', error);
       });
   };
   return (
